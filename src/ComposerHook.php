6803c544f9694140bfe032f5ef6a79868601e7f7
Kabir Baidhya <kabeer182010@gmail.com>
2015-04-09 15:58:17 +0545

    added composer hook

---
+++ src/ComposerHook.php
@@ -0,0 +1,19 @@
+<?php namespace Gckabir\Arty;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
+class ComposerHook
+{
+
+    public static function postPackageInstall(PackageEvent $event)
+    {
+        $package = $event->getOperation()->getPackage();
+        // do stuff
+
+        echo ' postPackageInstall: '.get_class($package);
+
+        echo ' name = '.$package->getName();
+        echo ' target dir = '.$package->getTargetDir();
+    }
+}
